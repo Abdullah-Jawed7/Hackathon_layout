5e7914ba0d6bbb5d1cfb1d8f9df78e2a8986fa05
Abdullah-Jawed7 <oneandonly0o0.hhh7@gmail.com>
2025-07-25 19:08:34 +0500

    Adding toastify

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,8 @@ import RegisterForm from './pages/auth/register.jsx'
 import LoginForm from './pages/auth/Login.jsx'
 import AuthLayout from "./pages/auth/layout.jsx"
 import OtpVerification from './pages/auth/otpVerification.jsx'
-import ForgotPasswordForm from './pages/auth/forget.jsx'
-import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { Bounce, ToastContainer } from 'react-toastify'
 
 function App() {
   // const {theme} = useSelector(state => state?.theme)
@@ -24,13 +23,28 @@ function App() {
   return (
    <>
    <Routes >
+
     <Route element={<AuthLayout/>}>
     <Route path='/register' element={<RegisterForm/>}/>
     <Route path='/login' element={<LoginForm/>}/>
-    <Route path='/forgot' element={<ForgotPasswordForm/>}/>
     <Route path='/otp' element={<OtpVerification/>}/>
     </Route>
+
    </Routes>
+
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        transition={Bounce}
+      />
 
    </>
   )
