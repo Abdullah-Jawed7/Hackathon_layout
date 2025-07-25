01619e41e82f87c7b419857618ff266c4dfe6acb
Abdullah-Jawed7 <oneandonly0o0.hhh7@gmail.com>
2025-07-25 19:09:55 +0500

    creating redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-// import userSlice from "./slices/userSlice.js"
+import userSlice from "./slices/userSlice.js"
 // import themeSlice from "./slices/themeSlice.js"
 
 
 export const Store = configureStore({
   reducer: {
-    // users: userSlice,
+    user: userSlice,
     // theme:themeSlice
   }
 })
