61364c916477d723c41d77693a61567effb284cc
Abdullah-Jawed7 <oneandonly0o0.hhh7@gmail.com>
2025-07-25 19:09:27 +0500

    checking condition for auth route

--- src/pages/auth/layout.jsx
+++ src/pages/auth/layout.jsx
@@ -4,10 +4,10 @@ import { Outlet } from 'react-router'
 function AuthLayout() {
 
     // check user does not had token in localstorage or cookie
+   const token =  localStorage.getItem("accessToken")
+
   return (
-    <div className='w-full min-w-screen min-h-screen h-full mx-auto p-3  bg-slate-100 dark:bg-slate-700/80 flex items-center justify-center' >
-      <Outlet/>
-    </div>
+    token ?  <Navigate to="/" /> : <div className='w-full min-w-screen min-h-screen h-full mx-auto p-3  bg-slate-100 dark:bg-slate-700/80 flex items-center justify-center' >  <Outlet/> </div>
   )
 }
 
