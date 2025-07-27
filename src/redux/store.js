c0a5869d68c1ab31246c69842e56bdb64b63151e
Abdullah-Jawed7 <oneandonly0o0.hhh7@gmail.com>
2025-07-27 21:48:44 +0500

    initializing reducer in store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "./slices/userSlice.js"
-// import themeSlice from "./slices/themeSlice.js"
+import themeSlice from "./slices/themeSlice.js"
 
 
 export const Store = configureStore({
   reducer: {
     user: userSlice,
-    // theme:themeSlice
+    theme:themeSlice
   }
 })
